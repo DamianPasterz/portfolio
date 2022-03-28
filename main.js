c495aeac359350d3ee41506666a831ccabba7c7b
Damian Pasterz <damian.pasterz@gmail.com>
2022-03-28 13:35:47 +0200

    add scrollup button

--- main.js
+++ main.js
@@ -37,3 +37,22 @@ menuLinks.forEach((link) =>
     window.scrollTo({ top: section, behavior: 'smooth' });
   })
 );
+
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 820 || document.documentElement.scrollTop > 820) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
