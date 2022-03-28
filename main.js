98d4a7dfd8defb738bf9535364d0703fcca5478a
Damian Pasterz <damian.pasterz@gmail.com>
2022-03-28 14:59:01 +0200

    scrollup update

--- main.js
+++ main.js
@@ -41,11 +41,11 @@ menuLinks.forEach((link) =>
 
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 820 || document.documentElement.scrollTop > 820) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
